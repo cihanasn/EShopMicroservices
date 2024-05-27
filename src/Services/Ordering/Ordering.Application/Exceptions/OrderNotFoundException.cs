54baff25b6a48cd442e8f20e72c4d02c56330340
Cihan ASAN <cihan.asan@solmaz.com>
2024-05-27 11:41:55 +0300

    Order.Application Commands

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Ordering.Application.Exceptions
+{
+    internal class OrderNotFoundException : NotFoundException
+    {
+        public OrderNotFoundException(Guid id) : base("Order", id)
+        {
+        }
+
+
+    }
+}
